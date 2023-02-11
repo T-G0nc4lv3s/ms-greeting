8f9d853334aab6ff20dcf4c5184e546345d949e5
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-02-11 14:16:35 -0300

    ms-greeting project created

---
+++ ms-greeting/src/main/java/br/dev/tsantana/msgreeting/MsGreetingApplication.java
@@ -0,0 +1,13 @@
+package br.dev.tsantana.msgreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGreetingApplication.class, args);
+	}
+
+}
---
+++ ms-greeting/src/test/java/br/dev/tsantana/msgreeting/MsGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.tsantana.msgreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
